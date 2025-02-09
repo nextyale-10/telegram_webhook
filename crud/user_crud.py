503063846fdbc191733c534353ce7cb920240cb0
Yao Chen <yaochenqwer@gmail.com>
2025-02-09 13:36:29 +0800

    stub sql code

--- crud/user_crud.py
+++ crud/user_crud.py
@@ -4,20 +4,29 @@ from do.model import User
 import datetime
 
 def check_user_exists(telegram_id: int,db:Session) -> bool:
-    db_user  = db.query(User).filter(User.telegram_id == telegram_id).first()
-    if db_user:
-        return True
-    else:
+    try:
+        db_user  = db.query(User).filter(User.telegram_id == telegram_id).first()
+        if db_user:
+            return True
+        else:
+            return False
+    except:
         return False
 def create_user(telegram_id: int,db:Session):
-    db_user = User(telegram_id=telegram_id,last_active=datetime.datetime.now())
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+    try:
+        db_user = User(telegram_id=telegram_id,last_active=datetime.datetime.now())
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    except:
+        return None
 def update_user_activity_time(telegram_id: int,db:Session):
-    db_user = db.query(User).filter(User.telegram_id == telegram_id).first()
-    db_user.last_active = datetime.datetime.now()
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+    try:
+        db_user = db.query(User).filter(User.telegram_id == telegram_id).first()
+        db_user.last_active = datetime.datetime.now()
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    except:
+        None
